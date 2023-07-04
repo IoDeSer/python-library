a0e8d59d23a283ed2a2c4304fd8c1cd08649aff5
Maciej <mkuchcik@gmail.com>
2023-07-04 23:14:46 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='IoDeSer',
+    packages=find_packages(),
+    version='0.3.0',
+    description='Python library for de/serialization of .io file format.',
+    author='IoDeSer',
+    license='MIT'
+)
